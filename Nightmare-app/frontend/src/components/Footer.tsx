1e5fe2db986fbe689921b921ac3dfd2dbd7e1d42
Giovanna Momesso <gigimcdp@gmail.com>
2025-09-03 09:20:37 -0300

    Add files via upload

---
+++ Nightmare-app/frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black border-t border-red-700 p-6 text-center text-red-600 font-medium mt-12">
+      <p>&copy; 2025 Nightmare. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
